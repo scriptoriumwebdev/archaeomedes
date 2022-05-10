43214d59a6b0fa28aa96fbfe5b58aaecf602e0a8
Karol Sobolewski <karolsobolewski92@gmail.com>
2022-05-10 13:40:43 +0200

    Init project with CRA

---
+++ dev/blueprint-templates/component/__pascalCase_name__/__pascalCase_name__.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './{{pascalCase name}}.module.scss';
+
+const {{pascalCase name}} = () => {
+  return (
+    <div className={styles.root}>
+      <p>{{pascalCase name}}</p>
+    </div>
+  );
+};
+
+export default {{pascalCase name}};
