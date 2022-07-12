9f7200f7d6ddd19772de3463401686abb67ce06e
Karol Sobolewski <karolsobolewski92@gmail.com>
2022-07-12 13:43:49 +0200

    Fix key values for contact & Add animations for navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import './styles/bootstrap.scss';
 import './styles/global.scss';
@@ -12,6 +14,7 @@ import Services from './components/views/ServicesPage/ServicesPage';
 import ContactPage from './components/views/ContactPage/ContactPage';
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
   return (
     <div>
       <BrowserRouter>
